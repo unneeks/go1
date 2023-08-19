9b769ec9202fb25d20f3075cbb4b9c176246c9fb
unneeks <unneeks@yahoo.com>
2023-08-19 22:09:16 +0530

    init

---
+++ src/main/java/com/ml/experiments/go1/Go1Application.java
@@ -0,0 +1,13 @@
+package com.ml.experiments.go1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Go1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Go1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ml/experiments/go1/Go1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.experiments.go1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Go1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
